be4d3b14cd3aeefa88e4dcdd5907eeaf69a1a3d5
Nexnt <a.frey@n3tmaster.ch>
2021-09-04 19:15:52 +0000

    ..

--- 05_backprop/hw5.py
+++ 05_backprop/hw5.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 from tools import load_iris, split_train_test
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
-
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator
+import pyvista as pv
 def sigmoid(x: float) -> float:
     '''
     Calculate the sigmoid of x
@@ -238,14 +240,14 @@ y, dE1, dE2 = backprop(x, target_y, M, K, W1, W2)
 
 
 K = 3  # number of classes
-M = 16
+M = 6
 D = train_features.shape[1]
 np.random.seed(42)
 # Initialize two random weight matrices
 W1 = 2 * np.random.rand(D + 1, M) - 1
 W2 = 2 * np.random.rand(M + 1, K) - 1
 W1tr, W2tr, Etotal, misclassification_rate, last_guesses = train_nn(
-    train_features[:20, :], train_targets[:20], M, K, W1, W2, 100, 0.3)
+    train_features[:20, :], train_targets[:20], M, K, W1, W2, 500, 0.3)
 
 #print(misclassification_rate)
 
@@ -256,14 +258,14 @@ guesses = test_nn(test_features, M, K, W1tr, W2tr)
 
 #print(accuracy_score(test_targets, guesses))
 
-#print(confusion_matrix(test_targets, guesses))
+cmt= confusion_matrix(test_targets, guesses)
 
 
 def plotit():
     errors=[]
     misclass=[]
     iters= []
-    for iter in range(1,150,5):
+    for iter in range(1,500,5):
         W1tr, W2tr, Etotal, misclassification_rate, last_guesses = train_nn(train_features[:20, :], train_targets[:20], M, K, W1, W2, iter, 0.3)
         errors.append(Etotal[-1])
 
@@ -274,8 +276,150 @@ def plotit():
     plt.plot(iters,errors, label='Error')    
     plt.plot(iters,misclass, label='Misclassification rate')    
     plt.title('Error and Misclassification rate in relation to training iterations')
+    plt.legend(loc='upper right')
     plt.xlabel("Training iterations")
     plt.savefig("./05_backprop/1_1_1.png")
     plt.show()
     ...
-plotit()
+#plotit()
+
+
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig("./05_backprop/cmt.png")
+
+#plot_confusion_matrix(cmt, classes)
+
+
+def testit():
+    errors=[]
+    misclass=[]
+    iters= []
+    iter=200
+    nodess = []
+
+    acc = []
+
+
+    for nodes in range(1,20,1):
+        print(nodes)
+        for iter in range(1,500,10):
+            print(iter)
+
+            W1 = 2 * np.random.rand(D + 1, nodes) - 1
+            W2 = 2 * np.random.rand(nodes + 1, K) - 1
+
+            W1tr, W2tr, Etotal, misclassification_rate, last_guesses = train_nn(train_features[:20, :], train_targets[:20], nodes, K, W1, W2, iter, 0.3)
+            errors.append(Etotal[-1])
+
+            misclass.append(misclassification_rate[-1])
+            nodess.append(nodes)
+            guesses = test_nn(test_features, nodes, K, W1tr, W2tr)
+            iters.append(iter)
+            acc.append(accuracy_score(test_targets, guesses))
+
+
+    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    surf = ax.plot_trisurf(iters, nodess, acc, cmap=cm.coolwarm, linewidth=0, antialiased=False)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_zlim(0, 0.8)
+    plt.show()
+    '''
+    plt.plot(nodess,acc, label='Accuracy')    
+    plt.plot(nodess,errors, label='Errors')    
+    plt.plot(nodess,misclass, label='Misclassification rate')    
+    plt.title('Accuracy, Error and Misclassification rate in relation to NN Nodes')
+    plt.legend(loc='upper right')
+    plt.xlabel("Nodes")
+    plt.savefig("./05_backprop/Bonus_1.png")
+    plt.show()
+    '''
+#testit()
+
+def testit2():
+    errors=[]
+    misclass=[]
+    iters= []
+    iter=200
+    nodess = []
+    lrs=[]
+    acc = []
+    nodes =6
+
+
+    for lr in np.linspace(0.01,1,10):
+        print(lr)
+        for iter in range(1,500,20):
+            print(iter)
+
+            W1 = 2 * np.random.rand(D + 1, nodes) - 1
+            W2 = 2 * np.random.rand(nodes + 1, K) - 1
+
+            W1tr, W2tr, Etotal, misclassification_rate, last_guesses = train_nn(train_features[:20, :], train_targets[:20], nodes, K, W1, W2, iter, lr)
+            errors.append(Etotal[-1])
+
+            misclass.append(misclassification_rate[-1])
+            lrs.append(lr)
+            guesses = test_nn(test_features, nodes, K, W1tr, W2tr)
+            iters.append(iter)
+            acc.append(accuracy_score(test_targets, guesses))
+
+
+    #fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    #ax = plt.axes(projection='3d')
+
+    #ax.contour3D(iters, lrs, acc, 50, cmap='binary')    
+    #ax.scatter3D(iters, lrs, acc, c=acc, cmap='Greens');
+    #plt.show()
+    
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    surf = ax.plot_trisurf(iters, lrs, acc, cmap=cm.coolwarm, linewidth=0.2, antialiased=True)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    ax.set_zlim(0, 2)
+    plt.savefig("./05_backprop/Bonus_3.png")
+    plt.show()
+    
+    '''
+    plt.plot(iters,acc, label='Accuracy')    
+    plt.plot(iters,lrs, label='Lrs')    
+    #plt.plot(nodess,misclass, label='Misclassification rate')    
+    plt.title('Accuracy, Error and Misclassification rate in relation to NN Nodes')
+    plt.legend(loc='upper right')
+    plt.xlabel("Nodes")
+    #plt.savefig("./05_backprop/Bonus_1.png")
+    plt.show()
+    '''
+testit2()
---
+++ 06_neural_networks/hw61.py
@@ -0,0 +1,256 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+batch_size = 4
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
+                                          shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+
+
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+
+
+
+# %%
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+
+# %%
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+
+# %%
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+
+# %%
+PATH = './06_neural_networks/cifar_net.pth'
+torch.save(net.state_dict(), PATH)
+
+
+# %%
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+
+# %%
+PATH = 'cifar_net.pth'
+
+net = Net()
+net.load_state_dict(torch.load(PATH))
+
+
+# %%
+outputs = net(images)
+
+
+# %%
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
+
+
+# %%
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
+
+
+# %%
+# prepare to count predictions for each class
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+total_of_class = torch.Tensor()
+
+
+cmt = torch.zeros(len(classes),len(classes), dtype=torch.int64)
+
+# again no gradients needed
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        # collect the correct predictions for each class
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+            cmt[label, prediction] = cmt[label, prediction] + 1
+
+print(cmt)
+
+
+
+# print accuracy for each class
+
+print(correct_pred.items())
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count) / total_pred[classname]
+    print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
+                                                   accuracy))
+    
+
+
+# %%
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center", color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig("./cmt.png")
+
+plot_confusion_matrix(cmt, classes)
+
+
+# %%
+misclass = {classname: 0 for classname in classes}
+for (label, tot_count), (classname, correct_count) in zip(total_pred.items(), correct_pred.items()):
+    ...
+    misclass[label] = (tot_count-correct_count)/tot_count
+print()
+plt.bar(misclass.keys(), misclass.values(), align='center', alpha=0.5)
+plt.savefig("./06_neural_networks/1_1_1.png")
+plt.title('Misclassification rate picture recognition')
+plt.ylabel("Training iterations")
+
+
+# %%
+len(classes)
+
+
+# %%
+a = torch.tensor([2., 3.], requires_grad=True)
+b = torch.tensor([6., 4.], requires_grad=True)
+
+Q = 3*a**3 - b**2
+
+external_grad = torch.tensor([1., 1.])
+Q.backward(gradient=external_grad)
+
+print(9*a**2 == a.grad)
+print(-2*b == b.grad)
+
+
