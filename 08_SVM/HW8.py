eca0be19057ac9fbb03a2870e0557d9ea491c97b
Nexnt <a.frey@n3tmaster.ch>
2021-09-27 10:49:35 +0000

    ...

--- 08_SVM/HW8.py
+++ 08_SVM/HW8.py
@@ -1,4 +1,5 @@
 from matplotlib.colors import LinearSegmentedColormap
+from numpy.core.fromnumeric import mean
 import sklearn
 from tools import plot_svm_margin, load_cancer
 from sklearn import svm
@@ -8,7 +9,8 @@ from sklearn.metrics import (accuracy_score, precision_score,
 from matplotlib.offsetbox import AnchoredText
 import numpy as np
 import matplotlib.pyplot as plt
-
+from tabulate import tabulate
+import timeit
 
 def _plot_linear_kernel():
     X, t = make_blobs(n_samples=40, centers=2)
@@ -84,49 +86,69 @@ def _compare_gamma():
     clf.fit(X,t)
     
     _subplot_svm_margin(clf, X, t, 3, 1)
+    anchored_text = AnchoredText("Gamma: {}".format(clf.gamma) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
+
 
     clf = svm.SVC(kernel='rbf',C=1000, gamma=0.2)
     clf.fit(X,t)
     ...
     _subplot_svm_margin(clf, X, t, 3, 2)
+    anchored_text = AnchoredText("Gamma: {}".format(clf.gamma) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
 
     clf = svm.SVC(kernel='rbf',C=1000, gamma=2)
     clf.fit(X,t)
 
     _subplot_svm_margin(clf, X, t, 3, 3)
-    
+    anchored_text = AnchoredText("Gamma: {}".format(clf.gamma) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     plt.savefig("08_SVM/1_3_1.png")
     plt.show()
 
 
 def _compare_C():
     X, t = make_blobs(n_samples=40, centers=2, random_state=6)
-
+    plt.figure(figsize=(15,10)) 
     clf = svm.SVC(kernel='linear',C=1000)
     clf.fit(X,t)
 
     _subplot_svm_margin(clf, X, t, 5, 1)
-
+    anchored_text = AnchoredText("C: {}".format(clf.C) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     clf = svm.SVC(kernel='linear',C=0.5)
     clf.fit(X,t)
 
     _subplot_svm_margin(clf, X, t, 5, 2)
-
+    anchored_text = AnchoredText("C: {}".format(clf.C) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     clf = svm.SVC(kernel='linear',C=0.3)
     clf.fit(X,t)
+    
 
     _subplot_svm_margin(clf, X, t, 5, 3)
-
+    anchored_text = AnchoredText("C: {}".format(clf.C) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     clf = svm.SVC(kernel='linear',C=0.05)
     clf.fit(X,t)
 
     _subplot_svm_margin(clf, X, t, 5, 4)
-
+    anchored_text = AnchoredText("C: {}".format(clf.C) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     clf = svm.SVC(kernel='linear',C=0.0001)
     clf.fit(X,t)
 
     _subplot_svm_margin(clf, X, t, 5, 5)
-    
+    anchored_text = AnchoredText("C: {}".format(clf.C) , loc=3)
+    ax = plt.gca()
+    ax.add_artist(anchored_text)
     plt.savefig("08_SVM/1_5_1.png")
     plt.show()
 
@@ -145,23 +167,50 @@ def train_test_SVM(
 
     This function should return (accuracy, precision, recall)
     '''
+    start = timeit.default_timer()
     svc.fit(X_train,t_train)
+    
     y = svc.predict(X_test)
-    print(accuracy_score(t_test,y))
-    print(precision_score(t_test, y))
-    print(recall_score(t_test, y))
+    end = timeit.default_timer()
+    print(end-start)
+    return accuracy_score(t_test,y), precision_score(t_test, y), recall_score(t_test, y), (end-start)
 
 #_plot_linear_kernel()
-_compare_gamma()
-_compare_C()
+#_compare_gamma()
+#_compare_C()
 
-(X_train, t_train), (X_test, t_test) = load_cancer()
 
-svc = svm.SVC(kernel='linear',C=1000)
-train_test_SVM(svc, X_train, t_train, X_test, t_test)
 
-svc = svm.SVC(kernel='rbf',C=1000)
-train_test_SVM(svc, X_train, t_train, X_test, t_test)
+res=np.empty((3,4))
 
-svc = svm.SVC(kernel='poly',C=1000)
-train_test_SVM(svc, X_train, t_train, X_test, t_test)
+fcns=['linear', 'rbf', 'poly']
+
+
+
+for i in range(3):
+    accs, precs, recs, times = [], [], [], []
+    for n in range(20):    
+
+        (X_train, t_train), (X_test, t_test) = load_cancer()
+        svc = svm.SVC(kernel=fcns[i],C=1000)
+        acc, prec, rec, time = train_test_SVM(svc, X_train, t_train, X_test, t_test)
+        accs.append(acc)
+        precs.append(prec)
+        recs.append(rec)
+        times.append(time)
+        print(i,n)
+    res[i] = [mean(accs), mean(precs), mean(recs), mean(times)] 
+
+a=('acc',
+'prec',
+'rec',
+'time')
+
+fcns=(['linear'], 
+    ['rbf'], 
+    ['poly'])
+
+tab = np.append(np.array(fcns), res, axis=1)
+
+print (tabulate(tab, headers=a))
+
